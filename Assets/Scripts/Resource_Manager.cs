02091d51297a29f137541a78e994eed27c6501e0
Ryan <Hoodrn03@gmail.com>
2020-01-26 17:13:49 +0000

    Added Remove Functions
    I have enabled an amount of resources to be removed from the counter, this is in the form of a bool return value, if the resource would be reduced bellow 0 then the function returns false and isn't removed.

--- Assets/Scripts/Resource_Manager.cs
+++ Assets/Scripts/Resource_Manager.cs
@@ -66,12 +66,28 @@ public class Resource_Manager : MonoBehaviour
         }
     }
 
+    // Manage Gold
+
     public void m_AddToGold(float addAmount)
     {
         m_Gold += addAmount;
     }
 
-    public float m_GetGold() => m_Gold; 
+    public float m_GetGold() => m_Gold;
+
+    public bool m_RemoveGold(float removeAmount)
+    {
+        if (m_Gold - removeAmount >= 0)
+        {
+            m_Gold -= removeAmount;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // Manage Food
 
     public void m_AddToFood(float addAmount)
     {
@@ -80,10 +96,36 @@ public class Resource_Manager : MonoBehaviour
 
     public float m_GetFood() => m_Food;
 
+    public bool m_RemoveFood(float removeAmount)
+    {
+        if (m_Food - removeAmount >= 0)
+        {
+            m_Food -= removeAmount;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // Manage Iron 
+
     public void m_AddToIron(float addAmount)
     {
         m_Iron += addAmount;
     }
 
     public float m_GetIron() => m_Iron;
+
+    public bool m_RemoveIron(float removeAmount)
+    {
+        if(m_Iron - removeAmount >= 0)
+        {
+            m_Iron -= removeAmount;
+
+            return true; 
+        }
+
+        return false;
+    }
 }
