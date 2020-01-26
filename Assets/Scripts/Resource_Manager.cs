f7305c62e6e6b9c70175fe1184c4d82f0d7877e4
Ryan <Hoodrn03@gmail.com>
2020-01-26 17:07:46 +0000

    Added Resources
    I have added a basic resource tracker into the game. Right now there is only one (for the player) but a second could be added and given to the opponent (When they are created).
    
    I have also added in adders and getters. This will be used to allow for the player to modify their resource value to build items.

---
+++ Assets/Scripts/Resource_Manager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resource_Manager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_Owner = null;
+
+    [SerializeField]
+    float m_Gold = 0;
+
+    [SerializeField]
+    float m_Food = 0;
+
+    [SerializeField]
+    float m_Iron = 0;
+
+    [SerializeField]
+    Text m_GoldText;
+
+    [SerializeField]
+    Text m_FoodText;
+
+    [SerializeField]
+    Text m_IronText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_GoldText != null)
+        {
+            string l_GoldText = "Gold : ";
+
+            if (m_GoldText.text != l_GoldText + m_Gold.ToString())
+            {
+                m_GoldText.text = l_GoldText + m_Gold.ToString();
+            }
+        }
+
+        if (m_FoodText != null)
+        {
+            string l_FoodText = "Food : ";
+
+            if (m_FoodText.text != l_FoodText + m_Food.ToString())
+            {
+                m_FoodText.text = l_FoodText + m_Food.ToString();
+            }
+        }
+
+        if (m_IronText != null)
+        {
+            string l_IronText = "Iron : ";
+
+            if (m_IronText.text != l_IronText + m_Iron.ToString())
+            {
+                m_IronText.text = l_IronText + m_Iron.ToString();
+            }
+        }
+    }
+
+    public void m_AddToGold(float addAmount)
+    {
+        m_Gold += addAmount;
+    }
+
+    public float m_GetGold() => m_Gold; 
+
+    public void m_AddToFood(float addAmount)
+    {
+        m_Food += addAmount;
+    }
+
+    public float m_GetFood() => m_Food;
+
+    public void m_AddToIron(float addAmount)
+    {
+        m_Iron += addAmount;
+    }
+
+    public float m_GetIron() => m_Iron;
+}
