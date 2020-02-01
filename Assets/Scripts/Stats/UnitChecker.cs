e22a4bd4c7ac6e1ebbc8b3f581be77b803d3c60a
JEMLPro <58868026+JEMLPro@users.noreply.github.com>
2020-02-01 13:43:24 -0600

    UnitChecker added (error)
    it will check when the mouse is hovering over the unit

---
+++ Assets/Scripts/Stats/UnitChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitChecker : MonoBehaviour
+{       //unit is selected is true
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.mousePosition.x >= gameObject.transform.position.x && Input.mousePosition.y >= gameObject.transform.position.y && (Input.mousePosition.x <= gameObject.transform.position.x + gameObject.GetComponent<Renderer>().bounds.size.x && Input.mousePosition.y <= gameObject.transform.position.y + gameObject.GetComponent<Renderer>().bounds.size.y)))
+        {
+            Debug.Log("unit has mouse over it");
+
+            //open options menu
+        }
+
+    }
+}
