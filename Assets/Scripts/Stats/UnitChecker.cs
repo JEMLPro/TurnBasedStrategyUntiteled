a844a85181fc916d3697bd42d58e91fc16955970
Ryan <Hoodrn03@gmail.com>
2020-02-01 20:06:33 +0000

    Converted mouse pos
    I have added a conversion for the mouse position which will take its positon on the screen and place it in world space

--- Assets/Scripts/Stats/UnitChecker.cs
+++ Assets/Scripts/Stats/UnitChecker.cs
@@ -17,12 +17,21 @@ public class UnitChecker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((Input.mousePosition.x >= gameObject.transform.position.x && Input.mousePosition.y >= gameObject.transform.position.y && (Input.mousePosition.x <= gameObject.transform.position.x + gameObject.GetComponent<Renderer>().bounds.size.x && Input.mousePosition.y <= gameObject.transform.position.y + gameObject.GetComponent<Renderer>().bounds.size.y)))
+        Vector3 l_MousePos = Camera.main.ScreenToWorldPoint(
+                 new Vector3(Input.mousePosition.x,
+                 Input.mousePosition.y,
+                 Camera.main.nearClipPlane));
+
+        if ((l_MousePos.x >= gameObject.transform.position.x && l_MousePos.y >= gameObject.transform.position.y) && 
+            (l_MousePos.x <= gameObject.transform.position.x + gameObject.GetComponent<Renderer>().bounds.size.x && l_MousePos.y <= gameObject.transform.position.y + gameObject.GetComponent<Renderer>().bounds.size.y))
         {
             Debug.Log("unit has mouse over it");
 
             //open options menu
         }
+
+        Debug.Log("Mouse : " + l_MousePos.x + " " + l_MousePos.y);
+        Debug.Log("Unit : " + gameObject.transform.position.x + " " + gameObject.transform.position.y);
 
     }
 }
