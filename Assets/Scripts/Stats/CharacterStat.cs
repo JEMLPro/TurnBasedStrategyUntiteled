8c39f1d8041899503feee05d64af88169430aa8a
JEMLPro <58868026+JEMLPro@users.noreply.github.com>
2020-02-01 11:08:46 -0600

    Prefab Try (errors)
    this has some errors ryan should be able to fix them

---
+++ Assets/Scripts/Stats/CharacterStat.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Characterstat : MonoBehaviour
+{
+    public int maxHP = 100;
+    public int currentHP { get; private set; }
+
+
+
+    public Stat HP;
+
+    public Stat Attack;
+
+    public Stat AttackRange;
+
+    public Stat Defence;
+
+    public Stat BuildTime;
+
+    public Stat BuildCost;
+
+    public Stat Type;
+
+    public void TakeDamage(int damage)
+    {
+
+        damage -= Defence.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+
+        currentHP -= damage;
+        Debug.Log(transform.name + " Takes " + damage + " damage ");
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+
+
+    }
+
+    public virtual void Die()
+    {
+        //die in some way
+        //overwritten code
+        Debug.Log(transform.name + " died ");
+    }
+
+
+
+}
