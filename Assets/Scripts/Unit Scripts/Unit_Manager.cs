cfc3319042e9b2e39daae8e9cc99fd840c3370b1
Ryan <Hoodrn03@gmail.com>
2020-03-03 19:17:59 +0000

    Limited Unit Movement
    I have added a limit on the unit movement within the game. The unit can only move a number of spaces up to their movement range, if they reach this number they will be unable to move again. I have also added a wait button which appears when the unit is selected, this button will use up all of the unit's movement points preventing them from moving again.
    
    Next, I will add another unit/unit manager which will handle the opponent's units and add a combat system between them. This will also enable me to add in limits to prevent the player from doing anything during the opponents turn and not allow for the player to select multiple units and the opponent's units as well.

---
+++ Assets/Scripts/Unit Scripts/Unit_Manager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_Manager : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> m_UnitList;
+
+    [SerializeField]
+    GameObject m_UnitMenu; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_GetSelectedUnit() != null)
+        {
+            m_UnitMenu.SetActive(true);
+        }
+        else
+        {
+            m_UnitMenu.SetActive(false);
+        }
+    }
+
+    public void m_Wait()
+    {
+        for (int i = 0; i < m_UnitList.Count; i++)
+        {
+            if (m_UnitList[i].GetComponent<UnitStat>().m_GetSelected())
+            {
+                m_UnitList[i].GetComponent<Unit_Movement>().m_Wait(); 
+            }
+        }
+    }
+
+
+    public GameObject m_GetSelectedUnit()
+    {
+        GameObject l_ReturnValue = null; 
+
+        for(int i = 0; i < m_UnitList.Count; i++)
+        {
+            if(m_UnitList[i].GetComponent<UnitStat>().m_GetSelected())
+            {
+                l_ReturnValue = m_UnitList[i];
+
+                return l_ReturnValue; 
+            }
+        }
+
+        return l_ReturnValue;
+    }
+}
