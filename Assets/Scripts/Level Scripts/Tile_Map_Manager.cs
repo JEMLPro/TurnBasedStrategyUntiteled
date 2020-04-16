69939e56af97a0e1bfb3f1f1b7757a48ff4a1238
Ryan <Hoodrn03@gmail.com>
2020-04-16 18:08:31 +0100

    Added Level Switching
    I have added in int variable into the tile map manager class which will allow for the easy switching between levels within the game. as long as there is a level in the json file with a proper name then the level can be loaded.
    
    Next, I will look over my code and do some commenting.

--- Assets/Scripts/Level Scripts/Tile_Map_Manager.cs
+++ Assets/Scripts/Level Scripts/Tile_Map_Manager.cs
@@ -32,7 +32,7 @@ public class Levels
 public class Tile_Map_Manager : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> m_Grid;
+    List<GameObject> m_Grid = new List<GameObject>();
 
     [SerializeField]
     GameObject m_Cell;
@@ -43,13 +43,52 @@ public class Tile_Map_Manager : MonoBehaviour
     Levels m_ListOfLevels = new Levels();
 
     [SerializeField]
-    TextAsset m_LevelManagerJson; 
+    TextAsset m_LevelManagerJson;
+
+    [SerializeField]
+    int m_iLevelLoaded = 0; 
 
     private void Start()
     {
         m_LoadMapFromJSONFile(m_LevelManagerJson);
 
-        m_CreateTileMap(m_GetLevelFromName("Level One")); 
+    }
+
+    private void Update()
+    {
+        switch (m_iLevelLoaded)
+        {
+            case 0:
+                m_CreateTileMap(m_GetLevelFromName("Level Test"));
+
+                m_iLevelLoaded = -1;
+                break;
+
+            case 1:
+                m_CreateTileMap(m_GetLevelFromName("Level One"));
+
+                m_iLevelLoaded = -1;
+                break;
+
+            case 2:
+                m_CreateTileMap(m_GetLevelFromName("Level Two"));
+
+                m_iLevelLoaded = -1;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void m_ResetGrid()
+    {
+        for (int i = 0; i < m_Grid.Count; i++)
+        {
+            Destroy(m_Grid[i]); 
+        }
+
+        m_Grid.Clear(); 
     }
 
     public void m_CreateTileMap(int rows, int columns)
@@ -112,16 +151,32 @@ public class Tile_Map_Manager : MonoBehaviour
 
     public void m_CreateTileMap(Level levelToLoad)
     {
+        // Reset the grid to it's default state.
+
+        m_ResetGrid(); 
+
+        // Init local variables.
+
         float l_fSpacing = -1.0f;
 
         List<string> l_AdjustedTileConfig = new List<string>();
 
+        // Debugging - Output name of level being loaded. 
+
+        Debug.Log(levelToLoad.levelName + ": " + levelToLoad.description);
+
+        // Check the tile configuration for the level. 
+
         if (levelToLoad.tileConfig.Length > 0)
         {
+            // Split the tile config into usable data. 
+
             foreach (var item in levelToLoad.tileConfig)
             {
                 l_AdjustedTileConfig.AddRange(item.Split(','));
             }
+
+            // Create and store the grid using level's definitions. 
 
             for (int i = 0; i < levelToLoad.rows; i++)
             {
@@ -133,9 +188,15 @@ public class Tile_Map_Manager : MonoBehaviour
                 }
             }
 
+            // Loop through created the newly created map. 
+
             for (int k = 0; k < m_Grid.Count; k++)
             {
+                // Assign a parent to the cells in the level heirarchy for organisation purposes. 
+
                 m_Grid[k].transform.parent = gameObject.transform;
+
+                // Using tile config for level assign proper tiles. 
 
                 if (l_AdjustedTileConfig.Count >= k)
                 {
@@ -153,21 +214,33 @@ public class Tile_Map_Manager : MonoBehaviour
 
                         default:
 
+                            // If the tile config is out of range the cell will be assigned a null material. 
+
                             m_Grid[k].GetComponent<Cell_Manager>().m_SetTile(CellTile.none, m_Tiles[0]);
 
-                            Debug.Log("Using Default with Assigned Tile : " + int.Parse(l_AdjustedTileConfig[k]));
+                            Debug.LogError("Error Code 0001 : Unable to assign material. " + int.Parse(l_AdjustedTileConfig[k]));
 
                             break;
                     }
                 }
                 else
                 {
+                    // If tile config has not been assigned for this cell assign null material, allowing for visual repersentation in level for the error. 
+
                     m_Grid[k].GetComponent<Cell_Manager>().m_SetTile(CellTile.none, m_Tiles[0]);
+
+                    Debug.LogError("Error Code 0001 : Unable to assign material. ");
                 }
             }
+
+            // Debugging - Output when level finished loading. 
+
+            Debug.Log("Level: " + levelToLoad.levelName + " has been loaded. ");
         }
         else
         {
+            // This will output when the level cannot be loaded. 
+
             Debug.LogError("Error Code 0000 : Unable to load level from file. "); 
         }
     }
@@ -191,13 +264,21 @@ public class Tile_Map_Manager : MonoBehaviour
 
     public void m_LoadMapFromJSONFile(TextAsset m_LevelJson)
     {
+        Debug.Log("Loading " + m_LevelJson.name + " from file");
+
         // Override the new object to have the data from the json file. 
 
         m_ListOfLevels = JsonUtility.FromJson<Levels>(m_LevelJson.text);
 
-        // m_Levels.Add(l_Level);
+        // Debugging - Output the number of levels loaded from file 
 
-        Debug.Log(m_ListOfLevels.levels[0].levelName); 
+        Debug.Log("Number of levels loaded : " + m_ListOfLevels.levels.Count);
+
+        if (m_ListOfLevels.levels.Count <= 0)
+        {
+            Debug.LogError("Error Code 0002 : Unable to load file into game. ");
+        }
+
     }
 
 }
