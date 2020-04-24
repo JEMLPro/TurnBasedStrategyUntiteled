00d3eecc0534179baf0395234d44707f9392ae58
Ryan <Hoodrn03@gmail.com>
2020-04-24 19:09:43 +0100

    Another One (Unit manager)
    I have added another unit manager into the game; the manager will be used to control the units within the game. I have also created a turn manager and assigned each manager a owner. This system will allow for each of the managers to only function durring the owner's turn. I have tested this and the units can only move during their turn but I haven't implemened an end turn/turn switching system into the game.
    
    The next steps are to design a few different user interface designs get feedback and then implement one of them so that the user interaction works alongside the gameplay mechanics.

--- Assets/Scripts/Mechanics Scripts/Unit Actions/Unit_Manager.cs
+++ Assets/Scripts/Mechanics Scripts/Unit Actions/Unit_Manager.cs
@@ -10,48 +10,67 @@ public class Unit_Manager : MonoBehaviour
     [SerializeField]
     List<GameObject> m_UnitList;
 
-    bool m_bCheckRange = false; 
+    bool m_bCheckRange = false;
+
+    [SerializeField]
+    GameObject m_TurnManager;
+
+    [SerializeField]
+    CurrentTurn m_Owner = CurrentTurn.player;
 
     void Start()
     {
-        m_GameMap = GameObject.FindGameObjectWithTag("Map"); 
+        // If the game objects are not set find the objects through a assigned tag. 
+        m_GameMap = GameObject.FindGameObjectWithTag("Map");
+
+        if(m_GameMap == null)
+        {
+            Debug.LogError("Error code 0010 - Unable to assign game object in game: " + "Game Map. ");
+        }
+
+        m_TurnManager = GameObject.FindGameObjectWithTag("Turn_Manager"); 
+
+        if(m_TurnManager == null)
+        {
+            Debug.LogError("Error code 0010 - Unable to assign game object in game: " + "Turn Manager. ");
+        }
     }
 
     private void Update()
     {
-        m_UpdateUnitPosition(); 
+        // This will check if the over of the unit manager is the same as current turn player. 
+        if (m_Owner == m_TurnManager.GetComponent<Turn_Manager>().m_GetCurrentTurn())
+        {
+            m_UpdateUnitPosition();
+        }
     }
 
     void m_UpdateUnitPosition()
     {
+        // Loops through each unit in the list. 
         foreach (var unit in m_UnitList)
         {
             if (unit != null)
             {
                 // Update the position of the current selected unit. 
-
                 if (unit.GetComponent<Unit_Active>().m_GetActiveUnit() == true)
                 {
                     // If the unit can still has movemet points they can still move. 
-
                     if (unit.GetComponent<Unit_Movement>().m_GetCurrentMoveRange() > 0)
                     {
                         // If the range hasn't been checked for this unit, check their movement range. 
-
                         if (m_bCheckRange == false)
                         {
                             m_GameMap.GetComponent<Tile_Map_Manager>().m_CheckCellRange(unit.GetComponent<Unit_Movement>().m_GetCurrentMoveRange(),
                                 unit.GetComponent<Unit_Movement>().m_GetCurrentPosition());
 
-                            m_bCheckRange = true; 
+                            m_bCheckRange = true;
                         }
 
                         // Store the selected cell locally to check it exists. 
-
                         GameObject l_TempPosition = m_GameMap.GetComponent<Tile_Map_Manager>().m_GetSelectedCell();
 
                         // If the cell exists. 
-
                         if (l_TempPosition != null)
                         {
                             // Check the distance between the current cell and the new cell. 
@@ -65,7 +84,7 @@ public class Unit_Manager : MonoBehaviour
                             // If the unit has moved reset the cells back to their origional state. 
 
                             m_GameMap.GetComponent<Tile_Map_Manager>().m_ResetCellColours();
-                            
+
                             m_bCheckRange = false;
 
                         }
@@ -73,12 +92,11 @@ public class Unit_Manager : MonoBehaviour
                 }
 
                 // Assign a new position to a unit without a current position. 
-
-                if(unit.GetComponent<Unit_Movement>().m_GetCurrentPosition() == null)
+                if (unit.GetComponent<Unit_Movement>().m_GetCurrentPosition() == null)
                 {
                     GameObject l_TempPos = m_GameMap.GetComponent<Tile_Map_Manager>().m_GetCellUsingGridPosition(0, 0);
 
-                    if(l_TempPos != null)
+                    if (l_TempPos != null)
                     {
                         unit.GetComponent<Unit_Movement>().m_SetPosition(l_TempPos);
                     }
@@ -86,5 +104,6 @@ public class Unit_Manager : MonoBehaviour
             }
         }
     }
+    
 
 }
