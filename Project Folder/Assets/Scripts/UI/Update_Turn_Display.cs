1db9373e4c29d0efb8bb2daaeb01133b332ad601
Ryan <Hoodrn03@gmail.com>
2020-05-24 00:34:16 +0100

    Created Info Bar
    I have created a bar which will be used to hold the info for the game, at the moment I have created some placeholders for resource info and I have created a turn display, as well as updating it with the current turn player.
    
    Next, I will create a HQ object for both of the players.

---
+++ Project Folder/Assets/Scripts/UI/Update_Turn_Display.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Update_Turn_Display : MonoBehaviour
+{
+    [SerializeField]
+    Text m_TurnText;
+
+    string m_sPlayerTurnText = "Player's Turn";
+
+    string m_sAITurnText = "Opponent's Turn";
+
+    [SerializeField]
+    GameObject m_TurnManager = null;
+
+    private void Start()
+    {
+        m_TurnManager = GameObject.FindGameObjectWithTag("Turn_Manager");
+
+        m_TurnText = GameObject.FindGameObjectWithTag("Turn_Text").GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if(m_TurnManager.GetComponent<Turn_Manager>().m_GetCurrentTurn() == CurrentTurn.player)
+        {
+            if(m_TurnText.text != m_sPlayerTurnText)
+            {
+                m_TurnText.text = m_sPlayerTurnText;
+            }
+        }
+        else
+        {
+            if (m_TurnText.text != m_sAITurnText)
+            {
+                m_TurnText.text = m_sAITurnText;
+            }
+        }
+
+    }
+}
